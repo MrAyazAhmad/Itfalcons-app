7a7cfb1dd6a87e29c6fa2469e74751ca3790e3ed
Ayaz Ahmad <mrayazsiyal@gmail.com>
2021-05-19 13:03:24 +0500

    itfalcons-app arslan files add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Controller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+// Route::get('details', function () {
+//     return view('portfolio-details');
+// });
+
+Route::get('details', [Controller::class,'index']);
