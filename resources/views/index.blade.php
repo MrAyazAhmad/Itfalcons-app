1af258b1005f28ed4ecfc975cf375ae84ebfc74b
unknown <abdullrehman1998@gmail.com>
2021-06-02 18:54:54 +0500

    added aboutus data

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -100,12 +100,13 @@
 
           <div class="col-lg-6 video-box align-self-baseline" data-aos="zoom-in" data-aos-delay="100">
             <img src="{{URL::to('public/index/assets/img/about.jpg')}}" class="img-fluid" alt="">
-            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+            <a href="https://www.youtube.com/watch?v=iosEScQJpE4" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
           </div>
 
           <div class="col-lg-6 pt-3 pt-lg-0 content">
-            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-            <p class="font-italic">
+            <h3>Build or Scale up your Business By Our ExpertTeam</h3>
+            <br><br><br>
+            <!-- <p class="font-italic">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
               magna aliqua.
             </p>
@@ -114,11 +115,9 @@
               <li><i class="bx bx-check-double"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
               <li><i class="bx bx-check-double"></i> Voluptate repellendus pariatur reprehenderit corporis sint.</li>
               <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-            </ul>
+            </ul> -->
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+             Our commitment is to ensure the quality of service through offering consistent engagements.
             </p>
           </div>
 
@@ -136,11 +135,11 @@
             <div class="card">
               <img src="{{URL::to('public/index/assets/img/about-boxes-1.jpg')}}" class="card-img-top" alt="...">
               <div class="card-icon">
-                <i class="ri-brush-4-line"></i>
+                <i class="fa fa-star"></i>
               </div>
               <div class="card-body">
                 <h5 class="card-title"><a href="">Our Mission</a></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                <p class="card-text">Our mission “Commitment to excellence” putting us in continuous efforts to take our company on higher notes. In this technological era, <span id="dots_vission" class="inline">...</span><span id="more_vission"  style="display: none;">everyone wants faster, effective and quicker solutions to cultivate their businesses. From consulting till the final outcome, our technical experts provide best solutions to stand out your business. Experienced strategic planners advise you creative and unique strategies and solutions to manage your business in efficient and profitable way.</span><a style="cursor: pointer; pointer; color: ##FF4A17; font-weight: bold;" id="vission" class="toggle_btn less"> &nbsp&nbsp read more</a></p>
               </div>
             </div>
           </div>
@@ -148,11 +147,11 @@
             <div class="card">
               <img src="{{URL::to('public/index/assets/img/about-boxes-2.jpg')}}" class="card-img-top" alt="...">
               <div class="card-icon">
-                <i class="ri-calendar-check-line"></i>
+                <i class="fa fa-users"></i>
               </div>
               <div class="card-body">
-                <h5 class="card-title"><a href="">Our Plan</a></h5>
-                <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                <h5 class="card-title"><a href="">Our Team</a></h5>
+                <p class="card-text">IT Falcons consulting team is well-experienced and passionate to advise you what and how to use technology to better your company’s progress in efficient <span id="dots_team" >...</span><span id="more_team"  style="display: none;"> and productive way. Our team not only advise but also planes, design and install IT systems for their clients. We are working with industries and on commercial level to provide help and advice about their business and IT problems. </span><a style="cursor: pointer; color: #FF4A17; font-weight: bold;" id="team" class="toggle_btn less"> &nbsp&nbsp read more</a></p>
               </div>
             </div>
           </div>
@@ -163,8 +162,8 @@
                 <i class="ri-movie-2-line"></i>
               </div>
               <div class="card-body">
-                <h5 class="card-title"><a href="">Our Vision</a></h5>
-                <p class="card-text">Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet. </p>
+                <h5 class="card-title"><a href="">Trust</a></h5>
+                <p class="card-text">Our commitment is to ensure the quality of service offering and building trust through our approach of consistent engagement and <span id="dots_trust" class="inline">...</span><span id="more_trust"  style="display: none;"> feedback. </span><a style="cursor: pointer; pointer; color: ##FF4A17; font-weight: bold;" id="trust" class="toggle_btn less"> &nbsp&nbsp read more </p>
               </div>
             </div>
           </div>
